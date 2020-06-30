5fb6fb2b43c50582e3b58fd9266801d6ae80aadc
Sam Price <67292616+sprice3@users.noreply.github.com>
2020-06-30 14:14:01 -0400

    Update and rename frankenpoem.py to wonderpoem.py

---
+++ poem/wonderpoem.py
@@ -0,0 +1,59 @@
+import random
+
+emotionOne = ["happy","sad","calm","loud","anger","joy","disgust","shame","love","terrfied"]
+emotionTwo = ["peace","relief","compassion","nervous","confused","bitter","mad","shocked","relieved","jealous"]
+emotionNess = ["happiness","sadness","maddness","boredness","foolishness","hopefulness","silliness","stupidness","resentfulness","eagerness"]
+adjectiveOne = ["awful","beautiful","cold","condescending","cruel","gloomy", "mean","miserable","normal","passionate"]
+adjectiveTwo = ["lame","critical","egotistical","hateful","lame","anxious","boring","dim"]
+
+for i in range(1):
+    e1 = random.choice(emotionOne)
+    e2 = random.choice(emotionTwo)
+    e3 = random.choice(emotionNess)
+    a1 = random.choice(adjectiveOne)
+    a2 = random.choice(adjectiveTwo)
+    print("I wonder what it's like to feel " + e1 + " without feeling " + e2 + ".")
+
+for i in range(1):
+    e1 = random.choice(emotionOne)
+    e2 = random.choice(emotionTwo)
+    e3 = random.choice(emotionNess)
+    a1 = random.choice(adjectiveOne)
+    a2 = random.choice(adjectiveTwo)
+    print("Are you filled with a sense of " + e3 + "?")
+
+print('\n')
+
+for i in range(1):
+    e1 = random.choice(emotionOne)
+    e2 = random.choice(emotionTwo)
+    e3 = random.choice(emotionNess)
+    a1 = random.choice(adjectiveOne)
+    a2 = random.choice(adjectiveTwo)
+    print("I wonder how it feels to be " + e1 + " all the time.")
+
+for i in range(1):
+    e1 = random.choice(emotionOne)
+    e2 = random.choice(emotionTwo)
+    e3 = random.choice(emotionNess)
+    a1 = random.choice(adjectiveOne)
+    a2 = random.choice(adjectiveTwo)
+    print("Is the world as " + a1 + " as it seems?")
+
+print('\n')
+
+for i in range(1):
+    e1 = random.choice(emotionOne)
+    e2 = random.choice(emotionTwo)
+    e3 = random.choice(emotionNess)
+    a1 = random.choice(adjectiveOne)
+    a2 = random.choice(adjectiveTwo)
+    print("I wonder if we'll ever all feel " + e1 + ".")
+
+for i in range(1):
+    e1 = random.choice(emotionOne)
+    e2 = random.choice(emotionTwo)
+    e3 = random.choice(emotionNess)
+    a1 = random.choice(adjectiveOne)
+    a2 = random.choice(adjectiveTwo)
+    print("Connected by this, not " + a2 + ", but in it together.")
